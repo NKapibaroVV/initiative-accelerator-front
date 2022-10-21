32c6104552af911e77a6443750af0af4959b14d2
AlexC-ux <eunv2004@gmail.com>
2022-10-21 23:06:08 +0300

    "developed by" added

--- src/Modules/footer/index.tsx
+++ src/Modules/footer/index.tsx
@@ -1,5 +1,17 @@
+import { Button, Chip } from "@mui/material";
+import GitHubIcon from '@mui/icons-material/GitHub';
 
-function Footer(){
-    return<></>
+function Footer() {
+    return (
+        <div className="pb-3 mx-0" style={{
+            marginTop: "2rem"
+        }}>
+            <div className="row w-100 justify-content-center">
+                <div className="col-12 text-center">
+                    <Chip label="Разработал AlexC-ux" variant="outlined" component="a" href="https://github.com/AlexC-ux?tab=repositories" icon={<GitHubIcon/>}/>
+                </div>
+            </div>
+        </div>
+    )
 }
 export default Footer;
