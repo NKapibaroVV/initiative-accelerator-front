9ce083c39d23c05049a87fc50b2e7acb9dae1db7
AlexC-ux <eunv2004@gmail.com>
2022-10-30 12:46:18 +0300

    123

--- src/Pages/addPrivateInitiative/index.tsx
+++ src/Pages/addPrivateInitiative/index.tsx
@@ -41,7 +41,7 @@ function AddPrivateInitiativePage() {
         })
         console.log(selectedUsers)
 
-        if (titleRef.current!.value.length>0) {
+        if (titleRef.current!.value.length>0&&Object.keys(selectedUsers).length>0) {
             fetch(`${process.env.REACT_APP_BACKEND_SERVER_DOMAIN}/api/add_initiative/`, {
                 headers: {
                     'Content-Type': 'application/json'
@@ -86,7 +86,7 @@ function AddPrivateInitiativePage() {
     
             }))
         }else{
-            alert("Длинна заголовка меньше 1 символа!");
+            alert("Не выполнено, проверьте корректность заполнения полей!");
             setBtnDisabled(false);
         }
         
