d8fa2dc13ad67f5a837fc67d4218cbfbaee585e5
AlexC-ux <eunv2004@gmail.com>
2022-12-09 18:28:33 +0300

    upd

--- src/Pages/statistic/bigInitiativesStatistics/index.tsx
+++ src/Pages/statistic/bigInitiativesStatistics/index.tsx
@@ -1,43 +1,37 @@
+import { Button } from "@mui/material";
 import { useEffect, useState } from "react"
 import { useGlobalUserState } from "../../../Modules/User/User"
 
 export default function BigInitiativesStatistics() {
 
-    const [content, setContent] = useState("Загрузка...");
+    const [content, setContent] = useState(<>Загрузка...</>);
 
     let currentUser = useGlobalUserState();
-    useEffect(()=>{
+    useEffect(() => {
         fetch(`${process.env.REACT_APP_BACKEND_SERVER_DOMAIN}/api/getBigInitiativesStatistics`, {
             headers: {
                 'Content-Type': 'application/json'
             },
             method: "POST",
 
-            body: JSON.stringify({ token:currentUser.userParams.token })
-        }).then(resp=>resp.text().then((csv:string)=>{
-            let textFileAsBlob:Blob = new Blob([csv], {type:'text/plain'}); 
+            body: JSON.stringify({ token: currentUser.userParams.token })
+        }).then(resp => resp.text().then((csv: string) => {
+            /**
+             * let textFileAsBlob: Blob = new Blob([csv], { type: 'text/plain' });
             var downloadLink = document.createElement("a");
-    	downloadLink.download = "file.csv";
-    	downloadLink.innerHTML = "Download File";
-    	if (window.webkitURL != null)
-    	{
-    		// Chrome allows the link to be clicked
-    		// without actually adding it to the DOM.
-    		downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
-    	}
-    	else
-    	{
-    		// Firefox requires the link to be added to the DOM
-    		// before it can be clicked.
-    		downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-    		downloadLink.style.display = "none";
-    		document.body.appendChild(downloadLink);
-    	}
-    
-    	downloadLink.click();
+            downloadLink.download = "file.csv";
+            downloadLink.innerHTML = "Download File";
+            downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+            downloadLink.style.display = "none";
+            document.body.appendChild(downloadLink);
+            downloadLink.click();
+             */
+
+            function dataUrl(data: string) { return 'data:text/plain;charset=utf-8, ' + encodeURI(data); }
+            setContent(<Button href={dataUrl(csv)} download="statistics_excel.csv">Скачать</Button>)
         }))
-    },[])
+    }, [])
     return <>
-    {content}
+        {content}
     </>
 }
