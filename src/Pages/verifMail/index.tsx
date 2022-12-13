05204c0e145c46893d6c44173ef1d4c6d045868f
AlexC-ux <eunv2004@gmail.com>
2022-12-14 00:21:07 +0300

    email verif upd

--- src/Pages/verifMail/index.tsx
+++ src/Pages/verifMail/index.tsx
@@ -1,8 +1,12 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import IUser from "../../interfaces/user";
+import { useGlobalUserState } from "../../Modules/User/User";
 
 export default function VerifMail(){
     const { id,code} = useParams();
+
+    const currentUser = useGlobalUserState();
 
     useEffect(()=>{
         fetch(`${process.env.REACT_APP_BACKEND_SERVER_DOMAIN}/api/email/verif`, {
@@ -14,6 +18,11 @@ export default function VerifMail(){
             body: JSON.stringify({ id: id, code:code })
         }).then(resp=>resp.json().then((response:any)=>{
             if (!!response.msg) {
+                let newUser:IUser = {
+                    ...currentUser.userParams, 
+                    email_verified:1
+                }
+                localStorage.setItem("userData", JSON.stringify(newUser))
                 alert("Email подтверждён!")
                 document.location.replace("/cab")
             }else{
